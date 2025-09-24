37265058874771fb4c0f9f3d0c6409fda8697e0a
JLockeHemphill <lockehemphill@gmail.com>
2025-09-24 13:15:08 -0400

    FirstDraft

---
+++ Lab 2.1 Hemphill.py
@@ -0,0 +1,16 @@
+#Locke Hemphill
+#Period 8
+#9/23/25
+
+import os
+os.system("cls")
+
+first_name = input("What is your first name?: ")
+last_name = input("What is your last name?: ")
+print("\n")
+print("Welcome to the program,",first_name,last_name)
+print("Welcome to the program,",first_name,last_name,sep="\t")
+age = input("What is your age?: ")
+print("\n")
+print("Wow,",first_name,last_name,"you are",age,"years old",end=" *new line* ")
+print("Wow,",first_name,last_name,"you are ",age,"years old")
